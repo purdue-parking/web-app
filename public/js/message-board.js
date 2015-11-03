345484341173d0098ca935493f984f511a952fcc
Anthony Natoli <anatoli@purdue.edu>
2015-11-03 12:45:02 -0500

    Voting works on messages page, just need to hook up to api

--- public/js/message-board.js
+++ public/js/message-board.js
@@ -6,3 +6,37 @@ $('.message-field').click(function(){
 
 	window.location = window.location + '/' + msgID + '?message=' + $(this).html() + '&votes=' + row.find('#voteCount').html();
 });
+
+$('.up-btn').click(function(){
+	var grouping = $(this).closest('td');
+	var downBtn = grouping.find('.down-btn');
+	var voteCount = parseInt(grouping.find('#voteCount').html());
+
+	if(downBtn.prop('disabled')){
+		voteCount += 2;
+	}
+	else{
+		voteCount++;
+	}
+
+	downBtn.prop('disabled', false);
+	$(this).prop('disabled', true);
+	grouping.find('#voteCount').html(voteCount);
+});
+
+$('.down-btn').click(function(){
+	var grouping = $(this).closest('td');
+	var upBtn = grouping.find('.up-btn');
+	var voteCount = parseInt(grouping.find('#voteCount').html());
+
+	if(upBtn.prop('disabled')){
+		voteCount -= 2;
+	}
+	else{
+		voteCount--;
+	}
+
+	upBtn.prop('disabled', false);
+	$(this).prop('disabled', true);
+	grouping.find('#voteCount').html(voteCount);
+});
